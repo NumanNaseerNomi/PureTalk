0af8cc6e285adb574f2a3d4a18f78f7e0ea74323
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-13 01:33:14 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { return view('home'); });
+Route::get('/profile', function () { return view('profile'); });
 Route::get('/login', function () { return view('login'); });
 Route::redirect('/logout', '/login');
