e9d99fe36cbe397b53b088cec170666368baf391
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-13 02:15:28 +0500

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,15 +7,15 @@
                     <a class="nav-link" href="/login">Login</a>
                 </li>
                 @if(true)
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profile</a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item" href="/profile">{{"<- User Name ->"}}</a></li>
-                        <li><a class="dropdown-item" href="/adminPanel">Admin Panel</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="/logout">Logout</a></li>
-                    </ul>
-                </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profile</a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="/profile">{{"<- User Name ->"}}</a></li>
+                            <li><a class="dropdown-item" href="/adminPanel">Admin Panel</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                        </ul>
+                    </li>
                 @endif
             </ul>
         </div>
