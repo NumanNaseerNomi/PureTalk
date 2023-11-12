0175248adc40b7c3e3aecc1015884e3a9e5d351f
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-13 02:18:02 +0500

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,9 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PureTalk</title>
-        @vite(['resources/js/app.js', 'resources/css/app.css'])
-    </head>
-    <body data-bs-theme="dark">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PureTalk</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+<body data-bs-theme="dark">
