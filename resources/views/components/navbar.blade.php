b4ea258ecd0c0fa355626eb6709dc438459b292a
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-13 01:15:44 +0500

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,13 +1,20 @@
-<nav class="navbar bg-body-tertiary fixed-top">
+<nav class="navbar navbar-expand bg-body-tertiary fixed-top">
     <div class="container-fluid">
+        <a class="navbar-brand" href="/">PureTalk</a>
         <div>
-            <a class="navbar-brand" href="/">PureTalk</a>
-        </div>
-        <div>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">Login</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Menu</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/profile">{{"<- User Name ->"}}</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
