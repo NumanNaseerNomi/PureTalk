6e56781a2fb768bb7a5037786cb6ccbe270bc7ce
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-13 03:12:42 +0500

    Create register.blade.php

---
+++ resources/views/register.blade.php
@@ -0,0 +1,32 @@
+@include('layouts.header')
+@include('components.navbar')
+<div class="container text-center">
+    <div class="row justify-content-md-center">
+        <div class="col-md-auto">
+            <div class="card" style="width: 30rem;">
+                <div class="card-header">Register</div>
+                    <div class="card-body">
+                        <form method="POST" action="/register">
+                            <div class="form-floating mb-3">
+                                <input class="form-control" type="email" name="email" id="email">
+                                <label for="email">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" type="password" name="password" id="password">
+                                <label for="password">Password</label>
+                            </div>
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-primary" type="submit">Register</button>
+                            </div>
+                        </form>
+                        <hr/>
+                        <div class="d-grid gap-2">
+                            <a class="btn btn-outline-primary" href="/login" role="button">Login</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@include('layouts.footer')
