a6b41e43aa0679fe20c59a89c03c96f1f57cda10
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-13 02:06:36 +0500

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,6 +10,7 @@
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profile</a>
                     <ul class="dropdown-menu dropdown-menu-end">
                         <li><a class="dropdown-item" href="/profile">{{"<- User Name ->"}}</a></li>
+                        <li><a class="dropdown-item" href="/adminPanel">Admin Panel</a></li>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="/logout">Logout</a></li>
                     </ul>
