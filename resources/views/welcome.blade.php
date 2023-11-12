91adac37befc3fbd3bd12103bc5307524aafc85d
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-13 00:23:01 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,3 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap demo</title>
-        @vite(['resources/js/app.js', 'resources/css/app.css'])
-    </head>
-    <body>
-        <h1>Hello, world!</h1>
-    </body>
-</html>
+@include('layouts.header')
+<h1>Hello, world!</h1>
+@include('layouts.footer')
