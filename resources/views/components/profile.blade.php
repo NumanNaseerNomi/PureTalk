029f1a9263492954ef265733f10958310f3de57e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-14 23:15:07 +0500

    Update profile.blade.php

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -5,21 +5,21 @@
                 <form method="POST" action="/register">
                     <div class="row">
                         <div class="col-6">
-                    <div class="form-floating mb-3">
-                        <input class="form-control" type="email" name="email" id="email">
-                        <label for="email">Email address</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input class="form-control" type="password" name="password" id="password">
-                        <label for="password">Password</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation">
-                        <label for="passwordConfirmation">Confirm Password</label>
-                    </div>
-                    <div class="d-grid gap-2">
-                        <button class="btn btn-primary" type="submit">Register</button>
-                    </div>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" type="email" name="email" id="email">
+                                <label for="email">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" type="password" name="password" id="password">
+                                <label for="password">Password</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation">
+                                <label for="passwordConfirmation">Confirm Password</label>
+                            </div>
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-primary" type="submit">Register</button>
+                            </div>
                         </div>
                     </div>
                 </form>
