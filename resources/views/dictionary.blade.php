b1210e8ed082b4455357991ccf88e7f543e4e4ec
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-15 03:00:58 +0500

    Update dictionary.blade.php

--- resources/views/dictionary.blade.php
+++ resources/views/dictionary.blade.php
@@ -4,10 +4,14 @@
     <div class="row">
         @foreach (range(1, 10) as $number)
             <div class="col-md-3 mb-3">
-                <div class="card">
+                <div class="card text-center">
                     <div class="card-body">
                         <h5 class="card-title">==Word==</h5>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                            <button type="button" class="btn btn-outline-primary">Block</button>
+                            <button type="button" class="btn btn-outline-primary">Unblock</button>
+                            <button type="button" class="btn btn-outline-primary">Delete</button>
+                        </div>
                     </div>
                 </div>
             </div>
