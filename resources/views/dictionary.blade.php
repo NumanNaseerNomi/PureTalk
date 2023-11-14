277870ae62e63be4e7509664e415c4879d2e6827
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-15 03:33:07 +0500

    Update dictionary.blade.php

--- resources/views/dictionary.blade.php
+++ resources/views/dictionary.blade.php
@@ -14,18 +14,7 @@
             </div>
         </div>
         @foreach (range(1, 10) as $number)
-            <div class="col-md-3 mb-3">
-                <div class="card text-center">
-                    <div class="card-body">
-                        <h5 class="card-title">==Word==</h5>
-                        <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
-                            <button type="button" class="btn btn-outline-primary">Block</button>
-                            <button type="button" class="btn btn-outline-primary">Unblock</button>
-                            <button type="button" class="btn btn-outline-primary">Delete</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('components.dictionaryItem')
         @endforeach
     </div>
 </div>
