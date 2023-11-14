67dbacf1bfc8d7bbab3eff50fd66f0b05c01f4ae
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-15 01:04:01 +0500

    Update post.blade.php

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -7,19 +7,3 @@
         </blockquote>
     </div>
 </div>
-
-<div class="accordion" id="commentsAccordion">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Comments</button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#commentsAccordion">
-      <div class="accordion-body">
-        @foreach (range(1, 10) as $number)
-            <div class="text-break"><strong>==autour name==</strong>: ==comment content==</div>
-            <hr/>
-        @endforeach
-    </div>
-    </div>
-  </div>
-</div>
