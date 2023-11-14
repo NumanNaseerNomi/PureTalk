4c0d8c47e491d33d6bd20202b8ea5d8476964a16
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-15 00:24:42 +0500

    Create post.blade.php

---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-header">==post title==</div>
+    <div class="card-body">
+        <blockquote class="blockquote mb-0">
+            <p>==post content==</p>
+            <footer class="blockquote-footer">==autour name==</footer>
+        </blockquote>
+    </div>
+</div>
