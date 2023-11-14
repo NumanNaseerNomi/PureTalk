fd3e397b15060015310b1880dea11f51ed3ed405
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-14 23:17:25 +0500

    Update profile.blade.php

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -13,13 +13,6 @@
                                 <input class="form-control" type="password" name="password" id="password">
                                 <label for="password">Password</label>
                             </div>
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation">
-                                <label for="passwordConfirmation">Confirm Password</label>
-                            </div>
-                            <div class="d-grid gap-2">
-                                <button class="btn btn-primary" type="submit">Register</button>
-                            </div>
                         </div>
                         <div class="col-6">
                             <div class="form-floating mb-3">
@@ -40,10 +33,6 @@
                         </div>
                     </div>
                 </form>
-                <hr/>
-                <div class="d-grid gap-2">
-                    <a class="btn btn-outline-primary" href="/login" role="button">Login</a>
-                </div>
             </div>
         </div>
     </div>
