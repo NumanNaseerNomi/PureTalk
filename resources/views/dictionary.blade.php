1158c94024e4f7b2b7e8314098cff539b564064f
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-15 03:14:14 +0500

    Update dictionary.blade.php

--- resources/views/dictionary.blade.php
+++ resources/views/dictionary.blade.php
@@ -2,6 +2,17 @@
 @include('components.navbar')
 <div class="container">
     <div class="row">
+        <div class="col-md-3 mb-3">
+            <div class="card text-center">
+                <div class="card-header">Add Word</div>
+                <div class="card-body">
+                    <form class="input-group input-group-sm" method="POST" action="/">
+                        <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="button-addon1">
+                        <button class="btn btn-outline-secondary" type="submit" id="button-addon1">Save</button>
+                    </form>
+                </div>
+            </div>
+        </div>
         @foreach (range(1, 10) as $number)
             <div class="col-md-3 mb-3">
                 <div class="card text-center">
