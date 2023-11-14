72dd4c06a63219e2266fcae978488d9a895240bc
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-15 03:33:02 +0500

    Create dictionaryItem.blade.php

---
+++ resources/views/components/dictionaryItem.blade.php
@@ -0,0 +1,12 @@
+<div class="col-md-3 mb-3">
+    <div class="card text-center">
+        <div class="card-body">
+            <h5 class="card-title">==Word==</h5>
+            <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                <button type="button" class="btn btn-outline-primary">Block</button>
+                <button type="button" class="btn btn-outline-primary">Unblock</button>
+                <button type="button" class="btn btn-outline-primary">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
