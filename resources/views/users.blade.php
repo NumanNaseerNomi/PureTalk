f887b28e30e36364e73b21c017eceabee01566d9
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-15 04:25:32 +0500

    Update users.blade.php

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,6 +1,38 @@
 @include('layouts.header')
 @include('components.navbar')
 <div class="container">
-    <h1>Users Page</h1>
+    <div class="row">
+        <div class="col-md-3 mb-3">
+            <div class="card text-center">
+                <div class="card-header">Register Moderator</div>
+                <div class="card-body">
+                    <form method="POST" action="/register">
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="name" name="name" id="name">
+                            <label for="name">Name</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="email" name="email" id="email">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="password" name="password" id="password">
+                            <label for="password">Password</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation">
+                            <label for="passwordConfirmation">Confirm Password</label>
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button class="btn btn-primary" type="submit">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        @foreach (range(1, 10) as $number)
+            <!-- @include('components.dictionaryItem') -->
+        @endforeach
+    </div>
 </div>
 @include('layouts.footer')
