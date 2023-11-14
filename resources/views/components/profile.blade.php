2d31a0602ac122ff141b775a2cd0cc32c21e0082
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-14 23:57:58 +0500

    Update profile.blade.php

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -1,37 +1,37 @@
-    <div class="card">
-        <div class="card-header">PROFILE</div>
-            <div class="card-body">
-                <form method="POST" action="/register">
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="email" name="email" id="email">
-                                <label for="email">Old Password</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="password" name="password" id="password">
-                                <label for="password">Password</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation">
-                                <label for="passwordConfirmation">Confirm Password</label>
-                            </div>
+<div class="card">
+    <div class="card-header">PROFILE</div>
+        <div class="card-body">
+            <form method="POST" action="/register">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="email" name="email" id="email">
+                            <label for="email">Old Password</label>
                         </div>
-                        <div class="col-6">
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="name" name="name" id="name">
-                                <label for="name">Name</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="email" name="email" id="email">
-                                <label for="email">Email</label>
-                            </div>
-                            <div class="d-grid gap-2">
-                                <button class="btn btn-outline-primary btn-lg" type="submit">Save</button>
-                            </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="password" name="password" id="password">
+                            <label for="password">Password</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation">
+                            <label for="passwordConfirmation">Confirm Password</label>
                         </div>
                     </div>
-                </form>
-            </div>
+                    <div class="col-6">
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="name" name="name" id="name">
+                            <label for="name">Name</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="email" name="email" id="email">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button class="btn btn-outline-primary btn-lg" type="submit">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
+</div>
