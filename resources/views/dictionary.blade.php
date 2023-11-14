f1a6589f7f221cae8d2fe45bbd9404145e2d915e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-15 02:48:49 +0500

    Update dictionary.blade.php

--- resources/views/dictionary.blade.php
+++ resources/views/dictionary.blade.php
@@ -1,6 +1,17 @@
 @include('layouts.header')
 @include('components.navbar')
 <div class="container">
-    <h1>Dictionary Page</h1>
+    <div class="row">
+        @foreach (range(1, 10) as $number)
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">==Word==</h5>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 @include('layouts.footer')
