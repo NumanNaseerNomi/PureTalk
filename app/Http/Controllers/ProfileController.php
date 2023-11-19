d17cf0b94332e627c43258ad61bbf6ae8a97c515
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 03:21:41 +0500

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Helpers\TextSanitizer;
 
 class ProfileController extends Controller
 {
     public function index()
     {
         $posts = Post::where('user_id', 1)->latest()->get();
+        
+        $textSanitizer = new TextSanitizer();
+        $textSanitizer->sanitize($posts);
         
         return view('profile', compact('posts'));
     }
