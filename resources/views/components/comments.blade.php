3acf0298ec898eaee4c0f4b4f064333bada19409
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 00:53:12 +0500

    Update comments.blade.php

--- resources/views/components/comments.blade.php
+++ resources/views/components/comments.blade.php
@@ -1,9 +1,9 @@
-<div class="accordion" id="commentsAccordion">
+<div class="accordion" id="commentsAccordion{{ $post->id }}">
     <div class="accordion-item">
         <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">Comments</button>
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $post->id }}" aria-expanded="false" aria-controls="collapse{{ $post->id }}">Comments</button>
         </h2>
-        <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#commentsAccordion">
+        <div id="collapse{{ $post->id }}" class="accordion-collapse collapse" data-bs-parent="#commentsAccordion{{ $post->id }}">
             <div class="accordion-body">
                 @foreach (range(1, 10) as $number)
                     <div class="text-break"><strong>==autour name==</strong>: ==comment content==</div>
