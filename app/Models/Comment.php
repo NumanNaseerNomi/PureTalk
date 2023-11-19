3f6f46f5869dbf16b08e669c83f41957a6722380
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 01:27:43 +0500

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'post_id', 'content'];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
