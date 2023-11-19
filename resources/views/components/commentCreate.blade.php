488db6f3e8d9bf973a193e6e49b625274209232b
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 01:43:01 +0500

    Update commentCreate.blade.php

--- resources/views/components/commentCreate.blade.php
+++ resources/views/components/commentCreate.blade.php
@@ -1,4 +1,5 @@
-<form class="input-group mb-3" method="POST" action="/">
-    <textarea class="form-control" placeholder="Send new comment" aria-label="With textarea" aria-describedby="button-addon1" rows="1"></textarea>
+<form class="input-group mb-3" method="POST" action="{{ route('comment.create', ['post_id' => $post->id]) }}">
+    @csrf
+    <textarea class="form-control" name="content" placeholder="Send new comment" aria-label="With textarea" aria-describedby="button-addon1" rows="1"></textarea>
     <button class="btn btn-outline-secondary" type="submit" id="button-addon1">Send</button>
 </form>
