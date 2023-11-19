6b2634ad865a516bf3438477fbf3ecddad382840
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-19 18:33:31 +0500

    Update 2023_11_19_133152_create_dictionary_table.php

--- database/migrations/2023_11_19_133152_create_dictionary_table.php
+++ database/migrations/2023_11_19_133152_create_dictionary_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('dictionary', function (Blueprint $table) {
             $table->id();
+            $table->string('word');
+            $table->boolean('isBlock')->default(false);
             $table->timestamps();
         });
     }
