544e3cbcc27bb5ec90ec6a829b09db8e81ffb9ea
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 00:49:02 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,5 @@
         @include('components.post')
         <br/>
     @endforeach
-    <div class="d-grid gap-2 col-1 mx-auto">
-        <button class="btn btn-outline-primary" type="button">Load More</button>
-    </div>
-    <br/>
 </div>
 @include('layouts.footer')
