73a3354cb28fb265f34a71f1fe849be330bbc028
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 03:08:08 +0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Helpers\TextSanitizer;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $posts = Post::with(['user', 'comments.user'])->latest()->get();
+        
+        $textSanitizer = new TextSanitizer();
+        $textSanitizer->sanitize($posts);
         
         return view('home', compact('posts'));
     }
