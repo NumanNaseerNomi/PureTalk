1f93c0f5015c922b8cb5acfe6b3d1fcfb88b52fa
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-19 22:11:07 +0500

    Update dictionary.blade.php

--- resources/views/dictionary.blade.php
+++ resources/views/dictionary.blade.php
@@ -1,20 +1,17 @@
 @include('layouts.header')
 @include('components.navbar')
 <div class="container">
-    <div class="row">
-        <div class="col-md-3 mb-3">
-            <div class="card text-center">
-                <div class="card-header">Add Word</div>
-                <div class="card-body">
-                    <form class="input-group input-group-sm" method="POST" action="/dictionary/create">
-                        @csrf
-                        <input type="text" class="form-control" name="word" aria-label="Add word" aria-describedby="button-addon1" required>
-                        <button class="btn btn-outline-secondary" type="submit" id="button-addon1">Save</button>
-                    </form>
-                </div>
-            </div>
+    <div class="card text-center mb-3">
+        <div class="card-header">Add Word</div>
+        <div class="card-body">
+            <form class="input-group input-group-sm" method="POST" action="/dictionary/create">
+                @csrf
+                <input type="text" class="form-control" name="word" aria-label="Add word" aria-describedby="button-addon1" required>
+                <button class="btn btn-outline-secondary" type="submit" id="button-addon1">Save</button>
+            </form>
         </div>
-        <hr/>
+    </div>
+    <div class="row">
         @foreach($words as $word)
             @include('components.dictionaryItem')
         @endforeach
