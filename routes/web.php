68da1eebf466b1ad40d7f75f62b89dd0b211d53f
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-19 23:58:25 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\DictionaryController;
 
@@ -15,7 +16,7 @@ use App\Http\Controllers\DictionaryController;
 |
 */
 
-Route::get('/', function () { return view('home'); });
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/profile', function () { return view('profile'); });
 Route::get('/users', function () { return view('users'); });
 
