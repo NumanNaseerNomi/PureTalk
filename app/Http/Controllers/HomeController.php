e76748834969d3790610930c1670e553d228af6a
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 01:48:29 +0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest()->get();
+        $posts = Post::with('comments')->latest()->get();
         
         return view('home', compact('posts'));
     }
