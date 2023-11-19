0fd144965dd7049ee021d9214f32f424d9bf8adf
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-19 18:28:04 +0500

    Update dictionary.blade.php

--- resources/views/dictionary.blade.php
+++ resources/views/dictionary.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-header">Add Word</div>
                 <div class="card-body">
                     <form class="input-group input-group-sm" method="POST" action="/">
-                        <input type="text" class="form-control" name="word" aria-label="Add word" aria-describedby="button-addon1">
+                        <input type="text" class="form-control" name="word" aria-label="Add word" aria-describedby="button-addon1" required>
                         <button class="btn btn-outline-secondary" type="submit" id="button-addon1">Save</button>
                     </form>
                 </div>
