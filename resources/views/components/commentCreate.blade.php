0dd8462fd6136995d483ce97272c319adaefa76c
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 01:30:10 +0500

    Create commentCreate.blade.php

---
+++ resources/views/components/commentCreate.blade.php
@@ -0,0 +1,4 @@
+<form class="input-group mb-3" method="POST" action="/">
+    <textarea class="form-control" placeholder="Send new comment" aria-label="With textarea" aria-describedby="button-addon1" rows="1"></textarea>
+    <button class="btn btn-outline-secondary" type="submit" id="button-addon1">Send</button>
+</form>
