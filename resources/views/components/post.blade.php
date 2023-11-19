f6c5ef5817c888ef180ab9f258d920564a975375
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-11-20 00:48:31 +0500

    Update post.blade.php

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,8 +1,8 @@
 <div class="card">
-    <div class="card-header">==post title==</div>
+    <div class="card-header">{{ $post->title }}</div>
     <div class="card-body">
         <blockquote class="blockquote mb-0">
-            <p>==post content==</p>
+            <p>{{ $post->content }}</p>
             <footer class="blockquote-footer">==autour name==</footer>
         </blockquote>
     </div>
