6551c159bbb0f74aaa47c63c2b800dcfc04dbf10
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-21 04:45:53 +0500

    Update post.blade.php

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -5,7 +5,7 @@
         <blockquote class="blockquote mb-0">
             <footer class="blockquote-footer">{{ $post->user->name }}</footer>
         </blockquote>
-        @if(Auth::user()->role == 'moderator')
+        @if(Auth::check() && Auth::user()->role == 'moderator')
             <form method="POST" action="{{ route('post.delete', ['id' => $post->id]) }}" class="d-grid gap-2 d-md-flex justify-content-md-end">
                 @csrf
                 @method('DELETE')
