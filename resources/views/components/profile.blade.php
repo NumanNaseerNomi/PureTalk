d9ce47fe6493fa73cd5cd18ad8b8e74a934602df
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-21 02:24:07 +0500

    Update profile.blade.php

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -1,10 +1,9 @@
 <div class="card">
     <div class="card-header">PROFILE</div>
     <div class="card-body">
-        <form method="POST" action="/register">
-            <div class="row">
-                <div class="col-md-6">
-                    <form method="POST" action="/register">
+        <div class="row">
+            <div class="col-md-6">
+                <form method="POST" action="/register">
                     <div class="form-floating mb-3">
                         <input class="form-control" type="name" name="name" id="name" value="{{ $user->name }}">
                         <label for="name">Name</label>
@@ -16,9 +15,10 @@
                     <div class="d-grid gap-2">
                         <button class="btn btn-outline-primary" type="submit">Save</button>
                     </div>
-                    </form>
-                </div>
-                <div class="col-md-6">
+                </form>
+            </div>
+            <div class="col-md-6">
+                <form method="POST" action="/register">
                     <div class="form-floating mb-3">
                         <input class="form-control" type="email" name="email" id="email">
                         <label for="email">Old Password</label>
@@ -31,11 +31,11 @@
                         <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation">
                         <label for="passwordConfirmation">Confirm Password</label>
                     </div>
-                </div>
-                <div class="d-grid gap-2">
-                    <button class="btn btn-outline-primary" type="submit">Save</button>
-                </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-outline-primary" type="submit">Save</button>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </div>
