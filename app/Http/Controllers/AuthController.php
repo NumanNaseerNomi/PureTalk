8c1ad3f8d1af79f49656f40ca7478db5da950c6c
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-21 00:58:01 +0500

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AuthController extends Controller
 {
     public function showLoginPage()
     {
         return view('login');
+    }
+
+    public function login(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate(
+            [
+                'email' => ['required', 'email'],
+                'password' => ['required'],
+            ]
+        );
+ 
+        if(Auth::attempt($credentials))
+        {
+            $request->session()->regenerate();
+ 
+            return redirect()->intended('/');
+        }
+ 
+        return back()->withErrors(['email' => 'The provided credentials do not match our records.'])->onlyInput('email');
     }
 }
