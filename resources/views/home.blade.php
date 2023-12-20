adf91122b6bf30522e278dfe656e82a106e42d76
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-21 03:34:23 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @include('layouts.header')
 @include('components.navbar')
 <div class="container">
+    @include('components.alerts')
     @if(Auth::check())
         @include('components.postCreate')
         <br/>
