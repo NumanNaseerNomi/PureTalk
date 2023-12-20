45c5a7ee85b1b1b337f9c438b77895bc9a1ba4fa
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-21 03:24:10 +0500

    Update alerts.blade.php

--- resources/views/components/alerts.blade.php
+++ resources/views/components/alerts.blade.php
@@ -1,8 +1,15 @@
 
 @if($errors->any())
-    <ul class="alert alert-danger ps-4 text-start" role="alert">
+    <ul class="alert alert-danger alert-dismissible fade show ps-4 text-start" role="alert">
         @foreach($errors->all() as $error)
             <li>{{ $error }}</li>
         @endforeach
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </ul>
+@endif
+@if(session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
 @endif
