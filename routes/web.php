fe5e5caed4da1a88516bef9ae6128b13f073b2af
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-21 05:16:52 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::middleware(['guest'])->group(
     function()
     {
         Route::get('/register', [AuthController::class, 'showRegisterPage'])->name('registerPage');
+        Route::post('/register', [AuthController::class, 'register'])->name('register');
+
         Route::get('/login', [AuthController::class, 'showLoginPage'])->name('loginPage');
         Route::post('/login', [AuthController::class, 'login'])->name('login');
     }
