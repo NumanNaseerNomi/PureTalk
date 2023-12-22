1c55be4f0f654145c93b1de4e08c02a73d23a092
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-22 05:12:52 +0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('users');
+        $tab = $request->input('tab') ?? 'active';
+        
+        $query = User::where('id', '!=', Auth::id());
+        
+        if($tab == 'active')
+        {
+            $query->whereNotNull('email_verified_at')->whereNull('bannedTill')->orWhere('bannedTill', '<', now());
+        }
+        elseif($tab == 'pending')
+        {
+            $query->whereNull('email_verified_at');
+        }
+        elseif($tab == 'banned')
+        {
+            $query->whereNotNull('bannedTill')->where('bannedTill', '>', now());
+        }
+        
+        $users = $query->get();
+        
+        return view('users', compact('users'));
     }
 }
