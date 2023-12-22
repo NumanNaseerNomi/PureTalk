956ff9d8528259f35f2245d54f7f1a3ada8111f5
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-22 05:55:06 +0500

    Create moderatorCreate.blade.php

---
+++ resources/views/components/moderatorCreate.blade.php
@@ -0,0 +1,19 @@
+<div class="card">
+    <div class="card-header">Create New Post</div>
+    <div class="card-body">
+        <form method="POST" action="/post/create">
+            @csrf
+            <div class="form-floating mb-3">
+                <input class="form-control" type="text" name="title" id="title" required>
+                <label for="title">Title</label>
+            </div>
+            <div class="form-floating mb-3">
+                <textarea class="form-control" name="content" id="content" style="height: 100px" required></textarea>
+                <label for="content">Content</label>
+            </div>
+            <div class="d-grid gap-2">
+                <button class="btn btn-outline-primary" type="submit">Post</button>
+            </div>
+        </form>
+    </div>
+</div>
