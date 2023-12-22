049a927b8a0099ebbae24f3de33ac0d66c038a71
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-22 18:54:13 +0500

    Update users.blade.php

--- resources/views/components/users.blade.php
+++ resources/views/components/users.blade.php
@@ -14,7 +14,17 @@
         @foreach ($users as $user)
             <tr>
                 <th scope="row">{{ $user->id }}</th>
-                <td>{{ $user->name }}</td>
+                <td>
+                    <span class="collapse multi-collapse-{{ $user->id }} show" id="showName">{{ $user->name }}</span>
+                    <div class="collapse multi-collapse-{{ $user->id }}" id="editName">
+                        <form class="input-group input-group-sm" method="POST" action="{{ route('profile.update', ['id' => $user->id]) }}">
+                            @csrf
+                            @method('PUT')
+                            <input type="text" class="form-control" value="{{ $user->name }}" name="name" aria-label="Add name" aria-describedby="button-addon1" required>
+                            <button class="btn btn-outline-secondary" type="submit" id="button-addon1">Save</button>
+                        </form>
+                    </div>
+                </td>
                 <td>{{ $user->email }}</td>
                 @if(request('tab') == 'banned')
                     <td>{{ $user->bannedTill }}</td>
@@ -30,6 +40,7 @@
                         <form method="POST" action="{{ route('user.delete', ['id' => $user->id]) }}">
                             @csrf
                             @method('DELETE')
+                            <button class="btn btn-outline-primary btn-sm" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse-{{ $user->id }}" aria-expanded="false" aria-controls="showName editName">Edit</button>
                             <button type="submit" class="btn btn-outline-primary btn-sm">Delete</button>
                         </form>
                     @endif
