792df041cd803b712cc8b08fd84199a9a5c60097
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-22 05:50:51 +0500

    Update users.blade.php

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -10,39 +10,43 @@
             <a href="?tab=moderators" class="btn btn-outline-primary @if(request('tab') == 'moderators') active @endif">Moderators</a>
         @endif
     </div>
-    <div class="row">
-        <div class="col-md-3 mb-3">
-            <div class="card text-center">
-                <div class="card-header">Add Moderators</div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <div class="card text-center">
+        <div class="card-header">Add Moderator</div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <input class="form-control" type="text" name="role" id="role" value="Moderator" required hidden>
+                <div class="row">
+                    <div class="col-6">
                         <div class="form-floating mb-3">
                             <input class="form-control" type="text" name="name" id="name" required>
                             <label for="name">Name</label>
                         </div>
+                    </div>
+                    <div class="col-6">
                         <div class="form-floating mb-3">
                             <input class="form-control" type="email" name="email" id="email" required>
                             <label for="email">Email</label>
                         </div>
+                    </div>
+                    <div class="col-6">
                         <div class="form-floating mb-3">
                             <input class="form-control" type="password" name="password" id="password" required>
                             <label for="password">Password</label>
                         </div>
+                    </div>
+                    <div class="col-6">
                         <div class="form-floating mb-3">
                             <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation" required>
                             <label for="passwordConfirmation">Confirm Password</label>
                         </div>
-                        <input class="form-control" type="text" name="role" id="role" value="Moderator" required hidden>
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-primary" type="submit">Save</button>
-                        </div>
-                    </form>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-primary" type="submit">Save</button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
-        @foreach (range(1, 10) as $number)
-        @endforeach
     </div>
 </div>
 @include('layouts.footer')
