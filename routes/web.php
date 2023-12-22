dcc46c701fca0b333e5605d32ab6ca62dc1b6c5c
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-22 18:25:20 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::middleware(['auth'])->group(
 
         Route::post('/post', [PostController::class, 'create'])->name('post.create');
         Route::delete('/post', [PostController::class, 'delete'])->name('post.delete');
-        Route::post('/comment/create', [CommentController::class, 'create'])->name('comment.create');
+        Route::post('/comment', [CommentController::class, 'create'])->name('comment.create');
 
         Route::put('/password', [AuthController::class, 'updatePassword'])->name('password.update');
         Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
