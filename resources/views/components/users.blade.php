4a042aead94e148b687dc6f2c42c2f210ceb29b4
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-22 06:15:58 +0500

    Update users.blade.php

--- resources/views/components/users.blade.php
+++ resources/views/components/users.blade.php
@@ -4,7 +4,9 @@
             <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
-            <th scope="col">Banned Till</th>
+            @if(request('tab') == 'banned')
+                <th scope="col">Banned Till</th>
+            @endif
             <th scope="col">Actions</th>
         </tr>
     </thead>
@@ -14,7 +16,9 @@
                 <th scope="row">{{ $user->id }}</th>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
-                <td>{{ $user->bannedTill }}</td>
+                @if(request('tab') == 'banned')
+                    <td>{{ $user->bannedTill }}</td>
+                @endif
                 <td>Actions</td>
             </tr>
         @endforeach
