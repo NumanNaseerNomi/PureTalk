7f74f8e7a06531bfaa27ce6e6b7464eece918283
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-12-22 15:01:49 +0500

    Update profile.blade.php

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -21,17 +21,25 @@
             <div class="col-md-6">
                 <form method="POST" action="{{ route('password.update') }}">
                     @csrf
+                    <div class="row">
+                    <div class="col-md-12">
                     <div class="form-floating mb-3">
                         <input class="form-control" type="password" name="current_password" id="current_password" required>
                         <label for="current_password">Current Password</label>
                     </div>
+                    </div>
+                    <div class="col-md-6">
                     <div class="form-floating mb-3">
                         <input class="form-control" type="password" name="password" id="password" required>
                         <label for="password">Password</label>
                     </div>
+                    </div>
+                    <div class="col-md-6">
                     <div class="form-floating mb-3">
                         <input class="form-control" type="password" name="password_confirmation" id="passwordConfirmation" required>
                         <label for="passwordConfirmation">Confirm Password</label>
+                    </div>
+                    </div>
                     </div>
                     <div class="d-grid gap-2">
                         <button class="btn btn-outline-primary" type="submit">Save</button>
